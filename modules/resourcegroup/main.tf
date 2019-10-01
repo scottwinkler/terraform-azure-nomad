83c1e4100229f63c50970fcb9350d354e8ebc5ad
Scott Winkler <scottjwinkler@gmail.com>
2019-10-01 14:52:24 -0700

    Update main.tf

--- modules/resourcegroup/main.tf
+++ modules/resourcegroup/main.tf
@@ -12,18 +12,3 @@ resource "azurerm_resource_group" "resource_group" {
   name     = local.namespace
   location = var.location
 }
-
-# Enable Boot Diagnostics
-resource "random_string" "rand" {
-  length  = 24
-  special = false
-  upper   = false
-}
-
-resource "azurerm_storage_account" "storage_account" {
-  name                     = random_string.rand.result
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
